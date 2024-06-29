5252886549c7c74a9806d7531a73e540780edef6
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-06-29 16:18:56 -0700

    Final commit

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|_err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(_e) = minigrep::run(config) {
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
